875f779bef2744fddaa454ac0ccb61339879f927
n-f9 <ndferguson@protonmail.com>
2021-12-24 09:59:12 -0500

    djs update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -11,7 +11,7 @@ export declare const QuackJSUtils: {
         HasRole(member: DiscordJS.GuildMember, finder: string): Boolean;
         GiveRole(guild: DiscordJS.Guild, member: DiscordJS.GuildMember, finder: string): Promise<DiscordJS.GuildMember>;
         RemoveRole(guild: DiscordJS.Guild, member: DiscordJS.GuildMember, finder: string): Promise<DiscordJS.GuildMember>;
-        CreateChannel(guild: DiscordJS.Guild, name: string, options: DiscordJS.GuildChannelCreateOptions): Promise<DiscordJS.TextChannel | DiscordJS.NewsChannel | DiscordJS.CategoryChannel | DiscordJS.VoiceChannel | DiscordJS.StoreChannel | DiscordJS.StageChannel>;
+        CreateChannel(guild: DiscordJS.Guild, name: string, options: DiscordJS.GuildChannelCreateOptions): Promise<DiscordJS.CategoryChannel | DiscordJS.NewsChannel | DiscordJS.StageChannel | DiscordJS.StoreChannel | DiscordJS.TextChannel | DiscordJS.VoiceChannel>;
         DeleteChannel(guild: DiscordJS.Guild, finder: string): void;
         CreateCategory(guild: DiscordJS.Guild, name: string, options: Object): Promise<DiscordJS.CategoryChannel>;
         DeleteCategory(guild: DiscordJS.Guild, finder: string): void;
